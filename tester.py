4f9058292f827a776f89805bb4577630911f35c4
The Lafkpages Team <mail@lafkpages.tech>
2020-06-23 10:00:30 +0200

    Update tester.py

--- tester.py
+++ tester.py
@@ -2,4 +2,4 @@ from urlcompleter import URLCompleter
 
 comp = URLCompleter()
 
-print(comp.complete('lafkpages.tech', 'games/ourG/mine/index.php'))
+print(comp.complete('example.com', 'path', 'another_path', 'file.html'))
